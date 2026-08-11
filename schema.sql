8e74fd7b922adb80fbc397c875b818c8ace065e5
sergejzuravlev360-max <sergejzuravlev360@gmail.com>
2026-08-11 23:54:41 +0800

    add line

--- schema.sql
+++ schema.sql
@@ -1,4 +1,14 @@
 CREATE TABLE categories (
-    id INTEGER PRIMARY KEY
+    id INTEGER PRIMARY KEY,
     name TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE transactions (
+    id INTEGER PRIMARY KEY,
+    amount REAL NOT NULL CHECK (amount > 0),
+    type TEXT NOT NULL CHECK (type IN ('income', 'expense')),
+    category_id INTEGER NOT NULL,
+    description TEXT,
+    date TEXT NOT NULL DEFAULT (date('now')),
+    FOREIGN KEY (category_id) REFERENCES categories(id)
 );
